12c9f06acd8d4174d02f8970f07efcf9223e7755
Thulani <nalebaniokiy99@gmail.com>
2022-05-19 02:50:20 -0700

    seeShell

--- ga.c
+++ ga.c
@@ -2,19 +2,30 @@
 #include <unistd.h>
 
 /**
- * main - execve example
+ * main - fork example
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+	pid_t my_pid;
+	pid_t child_pid;
 
-	printf("Before execve\n");
-	if (execve(argv[0], argv, NULL) == -1)
+	child_pid = fork();
+	if (child_pid == -1)
 	{
 		perror("Error:");
+		return (1);
 	}
-	printf("After execve\n");
+	my_pid = getpid();
+	printf("My pid is %u\n", my_pid);
+	if (child_pid == 0)
+	{
+		printf("(%u) Nooooooooo!\n", my_pid);
+	}
+	else
+	{
+		printf("(%u) %u, I am your father\n", my_pid, child_pid);
+	}
 	return (0);
 }
